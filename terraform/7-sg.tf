c89115f07509db009c4a840f9915776027415d32
26nch <nchb1807838@gmail.com>
2025-06-05 13:19:15 +0700

    update sg

--- terraform/7-sg.tf
+++ terraform/7-sg.tf
@@ -9,20 +9,31 @@ resource "aws_security_group" "sg_k8s" {
     cidr_blocks = [ "0.0.0.0/0" ]
   }
   ingress {
-    from_port = 6443
+    from_port = 6443 # API Server
     to_port = 6443
     protocol = "tcp"
     cidr_blocks = [ "0.0.0.0/0" ]
   }
   ingress {
-    from_port = 2379
+    from_port = 2379 # etcd 
     to_port = 2379
     protocol = "tcp"
     cidr_blocks = [ "0.0.0.0/0" ]
   }
   ingress {
-    from_port = 10250
+    from_port = 10250 # kubelet API
     to_port = 10250
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  ingress {
+    from_port = 10251 # Scheduler
+    to_port = 10251
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  ingress {
+    from_port = 10252 # Kube-Controller-Manager
     protocol = "tcp"
     cidr_blocks = [ "0.0.0.0/0" ]
   }
